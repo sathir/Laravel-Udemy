f02209d5aa54413bbefd556511c21bb9938ca131
Sathir <sathir@live.com>
2020-09-03 15:56:33 +0530

    Created show todos

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -10,7 +10,8 @@ class DatabaseSeeder extends Seeder
      * @return void
      */
     public function run()
-    {
-        // $this->call(UserSeeder::class);
+    {  
+        //$this->call(TodoSeeder::class);
+        $this->call(UserSeeder::class);
     }
 }
--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -3,9 +3,36 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" crossorigin="anonymous">
+
     <title>Todos</title>
 </head>
 <body>
-    <h1>Todos Page</h1>
+    
+    <div class="container">
+    <h1 class="text-center my-5">Todos Page</h1>
+    <div class="row justfy-content-center">
+    <div class="col-md-8">
+    <div class="card card-default">
+    <div class="card-header">
+    Todos
+    </div>
+    <div class="card-body">
+    <ul class="list-group">
+    @foreach ($todos as $todo)
+    <li class="list-group-item">
+    {{$todo->name}}
+    <div class="btn btn-primary float-right btn-sm">
+    View
+    </div>
+    </li>
+    @endforeach
+    </ul>
+    </div>
+    </div>
+    </div>
+    </div>    
+    </div>
+   
 </body>
 </html>
