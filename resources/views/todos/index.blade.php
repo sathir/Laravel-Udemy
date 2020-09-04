39aae614fdb4651c82ed41cab17c15302b577d5f
Sathir <sathir@live.com>
2020-09-04 14:33:14 +0530

    Layouts created

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -1,38 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" crossorigin="anonymous">
+@extends('layouts.app')
 
-    <title>Todos</title>
-</head>
-<body>
-    
-    <div class="container">
-    <h1 class="text-center my-5">Todos Page</h1>
-    <div class="row justfy-content-center">
-    <div class="col-md-8">
-    <div class="card card-default">
-    <div class="card-header">
-    Todos
-    </div>
-    <div class="card-body">
-    <ul class="list-group">
-    @foreach ($todos as $todo)
-    <li class="list-group-item">
-    {{$todo->name}}
-    <div class="btn btn-primary float-right btn-sm">
-    View
-    </div>
-    </li>
-    @endforeach
-    </ul>
-    </div>
-    </div>
-    </div>
-    </div>    
-    </div>
-   
-</body>
-</html>
+@section('status')
+Todo List
+@endsection
+
+@section('content') 
+        <h1 class="text-center my-5">Todos Page</h1>
+        <div class="row justfy-content-center">
+            <div class="col-md-8">
+                <div class="card card-default">
+                    <div class="card-header">
+                        Todos
+                    </div>
+                    <div class="card-body">
+                        <ul class="list-group">
+                            @foreach ($todos as $todo)
+                            <li class="list-group-item">
+                                {{$todo->name}}
+                                <a href="\todos\{{$todo->id}}" class="btn btn-primary float-right btn-sm">
+                                    View
+                                </a>
+                            </li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+@endsection
